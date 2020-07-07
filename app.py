434bfe8e4156d6059afd4c469f7923d8e417385b
HyperbolicStudios <marke9135@sd61learn.ca>
2020-07-06 21:57:14 -0700

    Added projects page

--- app.py
+++ app.py
@@ -7,5 +7,8 @@ app = Flask(__name__)
 def home(name=None):
     return render_template('index.html', name=name)
 
+@app.route("name=None"):
+    return render_template('projects.html', name=name)
+
 if __name__ == "__main__":
     app.run(debug=True)
